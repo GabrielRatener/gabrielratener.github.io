7f28b6a32fd852b4f54647af8f57d40cf81e7d29
Gabriel Ratener <gabe9693@gmail.com>
2021-04-28 13:42:13 -0500

    changes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ad8d63da7006fc42a3fe";
+/******/ 	var hotCurrentHash = "5561ac71743c25533825";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
